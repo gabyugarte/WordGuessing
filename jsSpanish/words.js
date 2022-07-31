3eb64840845a18d223c0613d35f3fc3a7c899dca
gaby <gabrielaugartemaco@gmail.com>
2022-07-31 08:09:39 +0200

    nuevos cambios realizados

---
+++ jsSpanish/words.js
@@ -0,0 +1,118 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "lenguaje de programación"
+    },
+    {
+        word: "guitarra",
+        hint: "un instrumento musical"
+    },
+    {
+        word: "aspirar",
+        hint: "un propósito o intención"
+    },
+    {
+        word: "venus",
+        hint: "planeta de nuestro sistema solar"
+    },
+    {
+        word: "oro",
+        hint: "un metal precioso de color amarillo brillante"
+    },
+    {
+        word: "ebay",
+        hint: "sitio web de compras en línea"
+    },
+    {
+        word: "golang",
+        hint: "lenguaje de programación"
+    },
+    {
+        word: "codigo",
+        hint: "referente a la programación"
+    },
+    {
+        word: "matrix",
+        hint: "pelicula de ciencia ficción"
+    },
+    {
+        word: "bugs",
+        hint: "relacionado a la programación"
+    },
+    {
+        word: "avatar",
+        hint: "película épica de ciencia ficción"
+    },
+    {
+        word: "gif",
+        hint: "un formato específico de imágenes con movimiento"
+    },
+    {
+        word: "mental",
+        hint: "relacionado con la mente"
+    },
+    {
+        word: "mapa",
+        hint: "diagrama que representa un área geográfica"
+    },
+    {
+        word: "isla",
+        hint: "espacio de tierra rodeado de agua"
+    },
+    {
+        word: "hockey",
+        hint: "famoso juego al aire libre"
+    },
+    {
+        word: "ajedres",
+        hint: "relacionado a un juego mental"
+    },
+    {
+        word: "facebook",
+        hint: "aplicación de redes sociales"
+    },
+    {
+        word: "github",
+        hint: "plataforma de cósigo para programadores"
+    },
+    {
+        word: "png",
+        hint: "formagto de imagen"
+    },
+    {
+        word: "plata",
+        hint: "precioso metal blanco grisáceo"
+    },
+    {
+        word: "mobil",
+        hint: "dispositivo electrónico que usamos a diario"
+    },
+    {
+        word: "gpu",
+        hint: "componente del ordenador"
+    },
+    {
+        word: "java",
+        hint: "lenguaje de programación"
+    },
+    {
+        word: "google",
+        hint: "famoso motor de búsqueda"
+    },
+    {
+        word: "venice",
+        hint: "famosa ciudad de agua"
+    },
+    {
+        word: "excel",
+        hint: "producto de microsoft para windows"
+    },
+    {
+        word: "mysql",
+        hint: "sistema de base de datos relacional"
+    },
+    {
+        word: "flauta",
+        hint: "instrumento musical"
+    }
+]
